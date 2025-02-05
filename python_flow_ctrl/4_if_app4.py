57388404c8cf76c6877ec94b1c65325f5a22cf47
DogyeomPark <qkrehfud@gmail.com>
2025-02-05 11:19:39 +0900

    4_if_app4.py from https://opentutorials.org/module/5707/30588 lecture 4

--- python_flow_ctrl/4_if_app4.py
+++ python_flow_ctrl/4_if_app4.py
@@ -1,36 +1,11 @@
-# id와 pw를 모두 만족시켰을때 문구가 나오는 어플리케이션 만들기
-
+input_id = input('id:')
 id = 'egoing'
-pw = '135'
-
-while True:
-    input_id = input('id: ')
-    input_pw = input('pw: ')
-    if input_id == id and input_pw == pw:
-        print('Now you are ready to go!')
-        break
-    elif input_id != id and input_pw == pw:
-        print('id or pw is wrong')
-    elif input_id == id and input_pw != pw:
-        print('id or pw is wrong')
-    elif input_id != id and input_pw != pw:
-        print('id or pw is wrong')
-
-print('ok')
-# input_id = input('id: ')
-# id = 'egoing'
-# input_pw = input('pw: ')
-# pw = '135'
-#
-#
-# while id =
-#
-# if input_id == id:
-#     print('id is same')
-#     input_pw
-#     if input_pw == pw:
-#         print('pw is same Now you can access')
-#     else:
-#         print('wrong pw')
-#         print('input the pw: ', input_pw)
-# else:
+input_password = input('password:')
+password = '111111'
+if input_id == id:
+    if input_password == password:
+        print('Welcome')
+    else:
+        print('Wrong password')
+else:
+    print('Wrong id')
